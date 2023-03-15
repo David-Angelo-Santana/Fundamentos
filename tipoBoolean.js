761110414c14638b2d8ad64423e1655970c3e1f2
David Sanatana <111399288+David-Angelo-Santana@users.noreply.github.com>
2023-03-14 21:58:19 -0300

    arquivos_js

---
+++ tipoBoolean.js
@@ -0,0 +1,19 @@
+const primeiroNumero = 1;
+const segundoNumero = 2;
+console.log(primeiroNumero === segundoNumero); //false
+
+const numero01 = 5;
+const numero02 = 5;
+console.log(numero01 === numero02); // true
+
+const texto1 = "alura";
+const texto2 = "alera";
+
+console.log(texto1 === texto2);
+
+
+
+//****************//obs: comandos para o repositório git = {git init / git status /} *** depois os 4 passo //1-{git remote add origin + o endereço do repositório} 
+// 2-{git add .} 3-{git commit -m "Exemplos da aula 01"} 4-{git push origin master}
+
+
---
+++ tipoString.js
@@ -0,0 +1,17 @@
+//const texto1 ="Olá Mundo!" ;
+//const texto2 ="Tudo Bem?";
+
+//const senha = "senhaSuper123";
+//const stringDeNumeros = "12345";
+//const citacao = "O Davi disse oi";
+
+//console.log(citacao , texto1 , texto2 , senha , stringDeNumeros, citacao);
+
+//concatenação*******************///////////////
+const citacao = "Meu nome égit status "
+const nome = "David"
+
+console.log(citacao + nome);
+
+
+
